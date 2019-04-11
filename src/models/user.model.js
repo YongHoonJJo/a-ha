01d6a3e492068df90d85c73e673bed54819be811
YongHoonJJo <starctak@gmail.com>
2019-04-12 00:23:00 +0900

    #07 - User Password

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 'use strict'
 
+import bcrypt from 'bcrypt'
 import {
   uuid
 } from '../utils/uuid'
@@ -37,6 +38,12 @@ module.exports = (sequelize, DataTypes) => {
   }
 
   // hooks
+  User.beforeSave(async (user, options) => {
+    if (user.changed('password')) {
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(user.password, salt);
+    }
+  });
 
   // print
   User.prototype.toWeb = function () {
@@ -47,6 +54,6 @@ module.exports = (sequelize, DataTypes) => {
 
     return values
   }
-  
+
   return User
 }
