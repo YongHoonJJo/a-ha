b2a02e4aff283d611656af8de034d5dfe6152b6e
YongHoonJJo <starctak@gmail.com>
2019-04-12 17:47:00 +0900

    #10 - Redis

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,6 +4,7 @@ import bcrypt from 'bcrypt'
 import {
   uuid
 } from '../utils/uuid'
+import userCache from '../caches/user.cache'
 
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
@@ -45,6 +46,9 @@ module.exports = (sequelize, DataTypes) => {
       user.password = await bcrypt.hash(user.password, salt);
     }
   });
+
+  // 생성 후 캐시에 저장
+  User.afterSave((user, options) => userCache.store(user))
 
   // print
   User.prototype.toWeb = function() {
